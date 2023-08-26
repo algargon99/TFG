bd03ef526b46ca4634f2115b56601ed65d6df918
Alberto García González <a.gargon99@gmail.com>
2023-08-26 21:54:05 +0200

    Se actualiza la vista de actualizar contraseña

--- front-chorus/src/views/Pass.vue
+++ front-chorus/src/views/Pass.vue
@@ -3,39 +3,26 @@
     <span class="h1 text-white">Cambio de contrase&ntilde;a</span>
   </div>
   <div class="row mt-3 g-0">
-    <div class="col-md-6 offset-md-3">
-      <div class="card">
-        <div class="card-header bg-dark text-white text-center">
-          Cambiar contrase&ntilde;a
+    <div class="col-md-4 offset-md-4 bloque">
+      <form class="form" v-on:submit="cambiarPass()">
+        <div class="mb-3">
+          <span>Contrase&ntilde;a actual:</span>
+          <input type="password" required v-model="pass" id="pass" placeholder="Contraseña actual" class="form-control">
         </div>
-        <div class="card-body">
-          <form class="form" v-on:submit="cambiarPass()">
-            <div class="input-group mb-3">
-              <span class="input-group-text">Contrase&ntilde;a actual</span>
-              <input type="password" required v-model="pass" id="pass" placeholder="Contraseña actual"
-                class="form-control">
-            </div>
-            <div class="input-group mb-3">
-              <span class="input-group-text">Contrase&ntilde;a nueva</span>
-              <input type="password" required v-model="newpass" id="newpass" placeholder="Nueva contraseña"
-                class="form-control">
-            </div>
-            <div class="input-group mb-3">
-              <span class="input-group-text">Repetir contrase&ntilde;a nueva</span>
-              <input type="password" required v-model="renewpass" id="renewpass" placeholder="Repetir contraseña"
-                class="form-control">
-            </div>
-            <div class="d-grid col-6 mx-auto mb-3">
-              <button class="btn btn-warning"><i class="fa-solid fa-refresh"></i> Actualizar contrase&ntilde;a</button>
-            </div>
-          </form>
+        <div class="mb-3">
+          <span>Contrase&ntilde;a nueva:</span>
+          <input type="password" required v-model="newpass" id="newpass" placeholder="Nueva contraseña"
+            class="form-control">
         </div>
-      </div>
-    </div>
-    <div class="col-6 mx-auto my-3">
-      <router-link :to="{ path: '/datosUsuario' }" class='btn btn-danger'>
-        <i class="fa-solid fa-arrow-left"></i> Volver
-      </router-link>
+        <div class="mb-3">
+          <span>Repetir contrase&ntilde;a nueva:</span>
+          <input type="password" required v-model="renewpass" id="renewpass" placeholder="Repetir contraseña"
+            class="form-control">
+        </div>
+        <div class="d-grid col-4 mx-auto py-3">
+          <button class="btn btn-warning">Actualizar contrase&ntilde;a</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
